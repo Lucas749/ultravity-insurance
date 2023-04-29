8e425a25a21033443551d732898eae9177989359
Lucas <lucas.schneider96@web.de>
2023-04-29 23:08:32 +0800

    permit2 example code

---
+++ contract_setup_helper/config.py
@@ -0,0 +1,12 @@
+
+####################################################
+#Ultravity Insurance Parameters
+####################################################
+stakingToken = "0xc0A7F1B0c9988FbC123f688a521387A51596da47"
+poolAddress = "0x71C2468664b8c0c7d0ad0eA59C1fc1ddA15CDA7c"
+routerAddress = "XX"
+
+####################################################
+#General
+####################################################
+node_url = "https://rpc.testnet.mantle.xyz"
